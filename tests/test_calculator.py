f73ca50934558ca7ef9500c4eca9b9a4f7fe9cb6
GowthamKrishna <gowthamaddluri@gmail.com>
2023-11-07 23:21:17 +0000

    test cases

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,11 +3,44 @@ from src.calculator import add,mul,div,sub
 def test_add():
     assert add(1,1) == 2
 
+def test_add():
+    assert add(-1,1) == 0
+
+def test_add():
+    assert add(1,-1) == 0  
+
+def test_add():
+    assert add(-1,-1) == -2     
+
 def test_sub():
     assert sub(1,1) == 0
+
+def test_sub():
+    assert sub(-1,-1) == 0    
+
+def test_sub():
+    assert sub(1,-1) == 2
+
+def test_sub():
+    assert sub(-1,1) == -2         
 
 def test_div():
     assert div(1,1) == 1
 
+def test_div():
+    assert div(-1,-1) == 1
+
+def test_div():
+    assert div(1,-1) == -1
+
+def test_div():
+    assert div(-1,1) == -1
+
 def test_mul():
-    assert mul(1,1) == 1       
+    assert mul(1,1) == 1    
+def test_mul():
+    assert mul(-1,-1) == 1
+def test_mul():
+    assert mul(1,-1) == -1   
+def test_mul():
+    assert mul(-1,1) == -1            
